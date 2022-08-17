851f7d5f49c3e04f1a5bbaba51dca500d78a6194
InnovationGuru <60159085+InnovationGuru@users.noreply.github.com>
2022-08-17 15:11:14 -0400

    latest

--- utilities.py
+++ utilities.py
@@ -1,6 +1,3 @@
-from pyproj import CRS
-import geopandas as gpd
-
 import os
 import exifread
 
@@ -12,6 +9,9 @@ class Orthomosaic:
         self.filename = os.path.basename(file_loc)
         self.exif_tags = None
         self.utm_zone = None
+        self.epsg = None
+        self.offset_x = None
+        self.offset_y = None
 
     def read_exif_data(self):
 
@@ -21,5 +21,15 @@ class Orthomosaic:
                 utm_data = self.exif_tags['Image Tag 0x87B1'].values
                 if utm_data.startswith('WGS 84'):
                     self.utm_zone = utm_data[utm_data.find('zone') + 5:utm_data.find('|')]
+            if 'Image Tag 0x87AF' in self.exif_tags:
+                utm_data = self.exif_tags['Image Tag 0x87AF'].values
+                self.epsg = utm_data[-5]
+            if 'Image Tag 0x87AF' in self.exif_tags:
+                utm_data = self.exif_tags['Image Tag 0x87AF'].values
+                self.epsg = utm_data[-5]
+            if 'Image Tag 0x8482' in self.exif_tags:
+                utm_data = self.exif_tags['Image Tag 0x8482'].values
+                self.offset_x = utm_data[-3][0]
+                self.offset_y = utm_data[-2][0]
 
 
